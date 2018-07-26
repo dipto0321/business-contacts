89a5b433c312de3516c92dcedea6d8f53bf85696
DESKTOP-7RP6RR3\dipto <diptokmk47@gmail.com>
2018-07-26 17:40:15 +0600

    add firebase to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZ1I0agA5n-g-GqQSdUcfob8DSug72BkE",
+    authDomain: "businesscontacts-461d5.firebaseapp.com",
+    databaseURL: "https://businesscontacts-461d5.firebaseio.com",
+    projectId: "businesscontacts-461d5",
+    storageBucket: "businesscontacts-461d5.appspot.com",
+    messagingSenderId: "537755748373"
+  }
 };
