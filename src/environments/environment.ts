23d274582c603da63e291e54781c2e335831fc74
DESKTOP-7RP6RR3\dipto <diptokmk47@gmail.com>
2018-07-26 17:26:19 +0600

    Make Env Prod True

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   // Firebase Config
   firebase: {
       apiKey: 'AIzaSyAZ1I0agA5n-g-GqQSdUcfob8DSug72BkE',
